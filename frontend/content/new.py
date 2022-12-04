c63fcca6cbab12e611394763db79b81bd925f741
ala2q6 <lxrbckl@protonmail.com>
2022-12-04 17:58:46 -0600

    intro to program

--- frontend/content/new.py
+++ frontend/content/new.py
@@ -7,6 +7,7 @@ import dash_bootstrap_components as dbc
 def newFunction(pType: str):
     '''  '''
 
+    # note: since it's new, you're going to have to ask where to insert it #
     print('new', pType) # remove
 
     return None
